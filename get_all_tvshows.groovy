2439c6ceae94db7ce81ebb55567c8a427fbd69f7
Lucas Maurice <lucas.maurice@outlook.com>
2019-04-29 05:24:49 -0400

    add config file for renaming

---
+++ get_all_tvshows.groovy
@@ -0,0 +1,7 @@
+// Analyse and rename all tv shows
+['/storage/origin/' as File].eachMediaFolder{
+	rename(folder:it, output: '/storage/destination/', format: "Tv Show/{n}/{'Season '+s}/{n} - {sxe} - {t}", action: "MOVE", conflict: "OVERRIDE")
+}
+
+// A EXECUTER: 
+// /opt/filebot/filebot -script ./get_all_tvshows.groovy -r ; /opt/filebot/filebot -script fn:cleaner /storage/origin/ -r
