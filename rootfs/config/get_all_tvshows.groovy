353ca0fa21381aed311cb371328f586581e70fa5
Lucas Maurice <lucasmaurice@users.noreply.github.com>
2019-04-30 04:30:23 -0400

    Split movies and Tv Shows.

--- rootfs/config/get_all_tvshows.groovy
+++ rootfs/config/get_all_tvshows.groovy
@@ -1,7 +1,10 @@
 // Analyse and rename all tv shows
-['/storage/origin/' as File].eachMediaFolder{
+['/storage/origin/Sonarr-TvShows' as File].eachMediaFolder{
 	rename(folder:it, output: '/storage/destination/', format: "Tv Show/{n}/{'Season '+s}/{n} - {sxe} - {t}", action: "MOVE", conflict: "OVERRIDE")
 }
-
+// Analyse and rename all movies
+['/storage/origin/Radarr-Movies' as File].eachMediaFolder{
+	rename(folder:it, output: '/storage/destination/', format: "Movies/Radarr/{ny}/{ny}", action: "MOVE", conflict: "OVERRIDE")
+}
 // A EXECUTER: 
 // /opt/filebot/filebot -script /config/get_all_tvshows.groovy -r ; /opt/filebot/filebot -script fn:cleaner /storage/origin/ -r
