8430d9cfc5b19f2d1d3ba335a5a9ec261791ebcb
Lucas Maurice <lucas.maurice@outlook.com>
2019-05-07 00:02:15 -0400

    Cleanup grouvy script for analyse download folder.

--- rootfs/config/get_all_tvshows.groovy
+++ rootfs/config/get_all_tvshows.groovy
@@ -2,9 +2,8 @@
 ['/storage/origin/Sonarr-TvShows' as File].eachMediaFolder{
 	rename(folder:it, output: '/storage/destination/', format: "Tv Show/{n}/{'Season '+s}/{n} - {sxe} - {t}", action: "MOVE", conflict: "OVERRIDE")
 }
+
 // Analyse and rename all movies
 ['/storage/origin/Radarr-Movies' as File].eachMediaFolder{
 	rename(folder:it, output: '/storage/destination/', format: "Movies/Radarr/{ny}/{ny}", action: "MOVE", conflict: "OVERRIDE")
 }
-// A EXECUTER: 
-// /opt/filebot/filebot -script /config/get_all_tvshows.groovy -r ; /opt/filebot/filebot -script fn:cleaner /storage/origin/ -r
