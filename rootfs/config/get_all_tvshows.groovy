a02cf0d43969b4a95a9ef6d5f197fb8141be1f22
Lucas Maurice <lucas.maurice@outlook.com>
2019-05-07 01:24:36 -0400

    Add a folder for just copy file and let it seed

--- rootfs/config/get_all_tvshows.groovy
+++ rootfs/config/get_all_tvshows.groovy
@@ -7,3 +7,13 @@
 ['/storage/origin/Radarr-Movies' as File].eachMediaFolder{
 	rename(folder:it, output: '/storage/destination/', format: "Movies/Radarr/{ny}/{ny}", action: "MOVE", conflict: "OVERRIDE")
 }
+
+// Analyse and rename all tv shows
+['/storage/origin/Sonarr-TvShows-Seed' as File].eachMediaFolder{
+	rename(folder:it, output: '/storage/destination/', format: "Tv Show/{n}/{'Season '+s}/{n} - {sxe} - {t}", action: "KEEPLINK", conflict: "OVERRIDE")
+}
+
+// Analyse and rename all movies
+['/storage/origin/Radarr-Movies-Seed' as File].eachMediaFolder{
+	rename(folder:it, output: '/storage/destination/', format: "Movies/Radarr/{ny}/{ny}", action: "KEEPLINK", conflict: "OVERRIDE")
+}
