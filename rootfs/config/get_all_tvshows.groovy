6bb0a613983499f3ed6ac09f8d8e78389170202d
Lucas Maurice <lucas.maurice@outlook.com>
2019-05-07 03:46:47 -0400

    Change conflict to Auto.

--- rootfs/config/get_all_tvshows.groovy
+++ rootfs/config/get_all_tvshows.groovy
@@ -10,10 +10,10 @@
 
 // Analyse and rename all tv shows
 ['/storage/origin/Sonarr-TvShows-Seed' as File].eachMediaFolder{
-	rename(folder:it, output: '/storage/destination/', format: "Tv Show/{n}/{'Season '+s}/{n} - {sxe} - {t}", action: "KEEPLINK", conflict: "OVERRIDE")
+	rename(folder:it, output: '/storage/destination/', format: "Tv Show/{n}/{'Season '+s}/{n} - {sxe} - {t}", action: "KEEPLINK", conflict: "AUTO")
 }
 
 // Analyse and rename all movies
 ['/storage/origin/Radarr-Movies-Seed' as File].eachMediaFolder{
-	rename(folder:it, output: '/storage/destination/', format: "Movies/Radarr/{ny}/{ny}", action: "KEEPLINK", conflict: "OVERRIDE")
+	rename(folder:it, output: '/storage/destination/', format: "Movies/Radarr/{ny}/{ny}", action: "KEEPLINK", conflict: "AUTO")
 }
